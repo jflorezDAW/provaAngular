5ee73c08c4bed65e21dc69080d001bd6754e1b27
jflorez99 <50742783+jflorez99@users.noreply.github.com>
2020-10-13 15:24:52 +0200

    12/10/2020

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,70 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  //numeros : number[] = [];
-  // numeros = new Array <number>(20);
-  // comptadors = new Array <number>(10) ;
+export class BodyComponent {
 
-  array1 : number[] = [];
-  array2 : number[] = [];
-
-  constructor (){
-    // inicialitzar les dades
-      this.initData();
-
-      this.initCounters();
-
-      // this.computeHistograms();
-        
-
-      }
-
-    initData() {
-      //for (let index = 0; index < 1000; index++) {
-      //this.numeros.push(Math.round(Math.random()*20));
-      for (let index = 0; index < this.array1.length; index++) {
-        this.array1[index] = Math.round(Math.random() * 20);
-     }
-      for (let index = 0; index < this.array2.length; index++) {
-      this.array2[index] = Math.round(Math.random() * 20);
-      }
-  }
-
-    initCounters() {
-      for (let index = 0; index < this.comptadors.length; index++) {
-        this.comptadors[index] = 0;
-        
-      }
-    }
-
-    // computeHistograms(){
-      // for (let index = 0; index < this.numeros.length; index++) {
-        // this.comptadors[this.numeros[index]] += 1;
-    // }
-  // }
-
-    // trobar(){
-      // var trobat : boolean;
-      // var index = 0;
-      // while (index < this.numeros.length && !trobat ) {
-        // if (this.numeros[index] > this.numeros[index + 1]) {
-          // trobat = true;
-        // }
-        // else{
-          // index ++;
-        // }
-      // }
-    // }
-
-    ngOnInit(){
-    
-    }
-
-  }
-
+ array1:number[] = [];
+ array2:number[] = [];
+ array3 = new Array<number>(10);
+ numeros : number = 0;
+ flag: boolean = false;
+ mostrarV1: boolean = false;
+ mostrarV2: boolean = false;
+ mostrarV3: boolean = false;
   
+ constructor() {
+  this.initData();
+  this.initComparar();
+  this.initMirar();
+  
+  if(this.numeros==1){
+  this.flag=true;
+  }
+
+  }
+  initData(){
+    for (let index = 0; index < 1000; index++) {
+      this.array1.push(Math.random()* 20);
+    }
+    for (let index = 0; index < 1000; index++) {
+      this.array2.push(Math.random()* 20);
+    }
+  }
+
+  initComparar(){
+    for(let index = 0; index < 1000; index++){
+      if(Math.abs(this.array1[index]-this.array2[this.array2.length-1- index])<0.01){
+        this.array3[index]=0;
+      }
+      else if(this.array1[index]>this.array2[this.array2.length-1-index]){
+        this.array3[index]=1;
+      }
+      else if(this.array1[index]<this.array2[this.array2.length-1-index]){
+        this.array3[index]=-1;
+      }
+    }
+  }
+  initMirar(){
+    let index=0;
+    while (index<1000 && this.numeros==0) {
+      if(this.array3[index]==0){
+        this.numeros=1;
+      }
+     else{
+      index++;
+      }
+    }
+  }
+}
